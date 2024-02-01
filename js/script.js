71eb4528bb4dc8fd0bf0d3a78f9b03d210f3f29a
Christian <joaochristian03@gmail.com>
2024-02-01 14:42:36 -0300

    14:42

--- js/script.js
+++ js/script.js
@@ -1,5 +1,42 @@
 //efeito do botão voltar ao Topo
-
+function topo(){
+    window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: 'smooth'
+    })
+}
+    
 //Validação de Login
 
+function login(){
+    var logado = 0;
+    var usuario = document.getElementsByName('usuario')[0].value;
+    usuario = usuario.toLowerCase();
+    var senha = document.getElementById('senha').value;
+    senha = senha.toLowerCase();
+    if(usuario == "admin" && senha == "123456"){
+    window.location = "index.html";
+    logado = 1;
+    }
+    if(usuario == "admin" && senha !== "123456"){
+    alert("Senha Incorreta");
+    }
+
+    if(usuario !== "admin" && senha == "123456"){
+    alert("Nome de Usuário Incorreto");
+    }
+
+    if(usuario == "admin" && senha == "123456"){
+        alert("Bem-Vindo de Volta")
+    }
+
+}
+
 //Ativar alert no botão cadastrar
+
+function cadastro() {
+    alert("Cadastrado com sucesso!");
+    window.location.href = "index.html";
+
+}
